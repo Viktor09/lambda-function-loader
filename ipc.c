aefbdd5d9394519b0dd997ae565543b56ed02b24
Victor Mandescu <80954149+Viktor09@users.noreply.github.com>
2024-12-20 21:40:46 +0200

    Update ipc.c

--- ipc.c
+++ ipc.c
@@ -15,19 +15,17 @@ int create_socket(void)
 {
 	/* TODO: Implement create_socket(). */
 	int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
-	
 	if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
+	      perror("ERROR opening socket");
+	      exit(1);
    	}
 	
 	return sockfd;
-	
+
 }
-// 443
 
 int connect_socket(int fd)
-{	
+{
 	struct sockaddr_un addr;
 	memset(&addr, 0, sizeof(addr));
 	addr.sun_family = AF_UNIX;
@@ -36,10 +34,11 @@ int connect_socket(int fd)
 	int cs = connect(fd, &addr, sizeof(addr));
 	if(cs < 0){
 		perror("ERROR connecting socket");
-      	exit(1);
+	      	exit(1);
 	}
 	/* TODO: Implement connect_socket(). */
 	return cs;
+
 }
 
 ssize_t send_socket(int fd, const char *buf, size_t len)
@@ -48,11 +47,10 @@ ssize_t send_socket(int fd, const char *buf, size_t len)
 	int ss = send(fd, buf, len, 0);
 	if(ss < 0){
 		perror("ERROR sending socket");
-      	exit(1);
+      		exit(1);
 	}
-
-
 	return ss;
+
 }
 
 ssize_t recv_socket(int fd, char *buf, size_t len)
@@ -61,9 +59,10 @@ ssize_t recv_socket(int fd, char *buf, size_t len)
 	int rs = recv(fd, buf, len, 0);
 	if(rs < 0){
 		perror("ERROR reciving socket");
-      	exit(1);
+      		exit(1);
 	}
 	return rs;
+	
 }
 
 void close_socket(int fd)
